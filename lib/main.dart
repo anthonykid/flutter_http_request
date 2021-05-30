575a096f7c6afb4880e5271024e451cefa76f105
anthonykid <dickya630@gmail.com>
2021-05-30 22:54:59 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_http_request/post_result_model.dart';
+import 'package:flutter_http_request/user_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,39 +12,36 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  PostResult postResult = null; 
+  PostResult postResult = null;
+  User user = null;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.indigo[100],
         appBar: AppBar(
           title: Text("API Demo"),
           backgroundColor: Colors.indigo[800],
         ),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly, //Place the free space evenly between the children as well as before and after the first and last child.
+            mainAxisAlignment: MainAxisAlignment
+                .spaceEvenly, //Place the free space evenly between the children as well as before and after the first and last child.
             children: <Widget>[
-              Text((postResult != null) //if condition for if there is data that we want, then it will show us all that we requested
-                  ? postResult.id +
-                      " | " +
-                      postResult.name +
-                      " | " +
-                      postResult.job +
-                      " | " +
-                      postResult.created
+              Text((user !=
+                      null) //if condition for if there is data that we want, then it will show us all that we requested
+                  ? user.id + " | " + user.name
                   : "No Data"), //if there is no data then the text would be shown as No Data
               RaisedButton(
                 onPressed: () {
-                  PostResult.connecToAPI("Anthony", "Doctor").then((value) {
-                    //this is what we want to take from the db, we use connecToAPI to connect and request our following data
-                    postResult = value;
-                    setState(() {}); //Notify the framework that the internal state of this object has changed.
+                  User.connectToAPI("3").then((value) {
+                    user = value;
+                    setState(() {});
                   });
                 },
-                child: Text("POST"),
+                child: Text("GET"),
               )
             ],
           ),
